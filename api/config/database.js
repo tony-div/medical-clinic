ee2a767a7335abbbfe23429a4c9b94e18fc75ac1
sparklele <yereikavonkoch@gmail.com>
2025-11-25 04:35:31 +0200

    added docker containers and database connection

---
+++ api/config/database.js
@@ -0,0 +1,12 @@
+import { createPool } from "mysql2/promise";
+import dotenv from 'dotenv';
+
+dotenv.config();
+export const db = createPool({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    port: process.env.DATABASE_PORT || 3306,
+    connectionLimit: process.env.DATABASE_CONNECTION_LIMIT || 10
+});
--- api/index.js
+++ api/index.js
@@ -1,12 +1,35 @@
 import express from 'express';
+import { db } from "./config/database.js"
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.SERVER_PORT;
 
 app.get('/greet', (req, res) => {
   res.send('hello world');
 });
 
-app.listen(PORT, () => {
+app.listen(PORT,"0.0.0.0", () => {
   console.log(`Server listening on http://localhost:${PORT}`);
+});
+
+//db tests and sanity checks, feel free to delete --
+
+app.get('/apple', async (req, res) => {
+  try {
+    const [rows] = await db.query('SELECT 1 AS db_status');
+    res.json(rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'DB connection failed' });
+  }
+});
+
+app.get('/banana', async (req, res) => {
+  try {
+    const [rows] = await db.query('SHOW DATABASES');
+    res.json(rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'DB connection failed' });
+  }
 });
