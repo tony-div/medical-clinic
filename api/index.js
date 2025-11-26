5fae019fd2fc0b528701e86a57f7af1a4b7761e9
Tony George <tg764562@gmail.com>
2025-11-26 17:47:01 +0200

    fix: delete unrestricted db access

--- api/index.js
+++ api/index.js
@@ -11,25 +11,3 @@ app.get('/greet', (req, res) => {
 app.listen(PORT,"0.0.0.0", () => {
   console.log(`Server listening on http://localhost:${PORT}`);
 });
-
-//db tests and sanity checks, feel free to delete --
-
-app.get('/apple', async (req, res) => {
-  try {
-    const [rows] = await db.query('SELECT 1 AS db_status');
-    res.json(rows);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: 'DB connection failed' });
-  }
-});
-
-app.get('/banana', async (req, res) => {
-  try {
-    const [rows] = await db.query('SHOW DATABASES');
-    res.json(rows);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: 'DB connection failed' });
-  }
-});
