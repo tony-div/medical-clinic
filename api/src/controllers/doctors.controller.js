e4369e7e92158e82759f1d54ac71f66c56bf6158
Reem Naji <reemalee7@gmail.com>
2025-11-27 11:06:35 +0200

    get doctors by specialty endpoint

--- api/src/controllers/doctors.controller.js
+++ api/src/controllers/doctors.controller.js
@@ -26,6 +26,35 @@ export const getDoctors = async (req, res) => {
   }
 };
 
+export const getDoctorsBySpecialty = async (req, res) => {
+  const specialtyId = req.params.specialtyId;
+  const query = `
+  Select Doctor.id, User.name AS name, Specialty.name AS specialty, profile_pic_path, DoctorRating.avg_rating
+    FROM DOCTOR
+    JOIN User ON Doctor.user_id = User.id 
+    JOIN Specialty ON Doctor.specialty_id = Specialty.id
+    JOIN DoctorRating ON Doctor.rating_id = DoctorRating.id
+    WHERE Doctor.specialty_id = ?
+    `
+    try{
+    const [rows, fields] = await db.query(query, [specialtyId]);
+
+    res.status(200).json({
+      message: 'ok',
+      data: rows
+    })
+
+  } catch(error){
+    console.error('error',error);
+    res.status(500).json({
+      message: 'Internal server error',
+      'error':error.message
+    })
+
+  }
+};
+
 export const getDoctorById = (req, res) => {
+  const doctorId = req.query.doctorId;
   res.status(501).json({ message: 'Not implemented' });
 };
--- api/src/routes/doctors.route.js
+++ api/src/routes/doctors.route.js
@@ -2,12 +2,15 @@ import express from 'express';
 import {
   getDoctors,
   getDoctorById,
+  getDoctorsBySpecialty
 } from '../controllers/doctors.controller.js';
 
 const doctorsRouter = express.Router();
 
 doctorsRouter.get('/', getDoctors);
 
-doctorsRouter.get('/:doctorId', getDoctorById);
+doctorsRouter.get('/profile/:doctorId', getDoctorById);
+
+doctorsRouter.get('/specialty/:specialtyId', getDoctorsBySpecialty);
 
 export default doctorsRouter;
