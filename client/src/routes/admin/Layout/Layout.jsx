78799aae7d9e7513dea6b97e71f735ef46e651f9
Eunoiaex <tibyanuwu@gmail.com>
2025-12-06 00:14:25 +0200

    fixed admin sidebar

--- client/src/routes/admin/Layout/Layout.jsx
+++ client/src/routes/admin/Layout/Layout.jsx
@@ -1,29 +1,26 @@
 import React from 'react';
-
 import Sidebar from '../Sidebar/Sidebar';
 import Navbar from '../Navbar/Navbar';
-
 import './Layout.css';
 
 const Layout = ({ children, activePage, setActivePage, isSidebarOpen, toggleSidebar }) => {
   return (
     <div className="app-wrapper">
-      {/* Mobile overlay - closes sidebar when clicked */}
-      <div
-        className={`sidebar-overlay ${isSidebarOpen ? 'visible' : ''}`}
-        onClick={toggleSidebar}
+      {/* 1. Sidebar */}
+      <Sidebar 
+        activePage={activePage} 
+        setActivePage={setActivePage} 
+        isOpen={isSidebarOpen} 
       />
-
-      <Sidebar
-        activePage={activePage}
-        setActivePage={setActivePage}
-        isOpen={isSidebarOpen}
-      />
-
+      
+      {isSidebarOpen && (
+        <div className="sidebar-overlay" onClick={toggleSidebar}></div>
+      )}
+      
       <div className={`main-container ${isSidebarOpen ? 'expanded' : 'collapsed'}`}>
-        <Navbar
-          activePage={activePage}
-          toggleSidebar={toggleSidebar}
+        <Navbar 
+          activePage={activePage} 
+          toggleSidebar={toggleSidebar} 
         />
         <main className="content-area">
           {children}
